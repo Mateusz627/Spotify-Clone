74943d8e08d06c22ae9328910f1e8dc4c467f2da
Mateusz Siuda <131684931+Mateusz627@users.noreply.github.com>
2023-08-24 15:07:27 +0200

    adding buttons

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -108,18 +108,34 @@ const Header: React.FC<HeaderProps> = ({
                   <>
                     <div>
                         <Button
+                            onClick={() => {}}
                             className="
                                 bg-transparent
                                 text-neutral-300
                                 font-medium
                             "
+
+
                         >
                             Sign up
                         </Button>
                     </div>
+                      <div>
+                          <Button
+                              onClick={() => {}}
+                              className="
+                                bg-white
+                                px-6
+                                py-2
+                            "
+                          >
+                              Log in
+                          </Button>
+                      </div>
                   </>
               </div>
           </div>
+          {children}
       </div>
   );
 }
