b27a5954474a140723078daf789e33cfd5d09b39
Mateusz Siuda <131684931+Mateusz627@users.noreply.github.com>
2023-08-29 15:45:26 +0200

    Added Input.tsx containing Input component using forwardRef and CSS styling with tailwind-merge library.

--- app/components/UploadModal.tsx
+++ app/components/UploadModal.tsx
@@ -1,15 +1,37 @@
 import Modal from "@/app/components/Modal";
 import useUploadModal from "@/hooks/useUploadModal";
+import {FieldValues, SubmitHandler, useForm} from "react-hook-form";
+import Input from "./Input";
+import {useState} from "react";
 
 const UploadModal = () => {
+    const [isLoading, setIsLoading] = useState();
     const uploadModal = useUploadModal();
+
+    const {
+        register,
+        handleSubmit,
+        reset
+    } = useForm<FieldValues>({
+        defaultValues: {
+            author: '',
+            title: '',
+            song: null,
+            image: null,
+        }
+    })
 
     const onChange = (open:boolean) => {
         if (!open) {
-            //Reset the form
+            reset()
             uploadModal.onClose();
         }
     }
+
+    const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+        //Upload to supabase
+    }
+
     return (
         <Modal
             title="Add a song"
@@ -17,7 +39,22 @@ const UploadModal = () => {
             isOpen={uploadModal.isOpen}
             onChange={onChange}
         >
-            Form
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                <Input
+                    id="title"
+                    disabled={isLoading}
+                    {...register('title', { required: true })}
+                    placeholder={"Song title"}
+                />
+                <Input
+                    id="author"
+                    disabled={isLoading}
+                    {...register('author', { required: true })}
+                    placeholder={"Song author"}
+                />
+            </form>
         </Modal>
     );
 }
