a06c3a5dacd0a1860e30c5b06896b10d08620332
Mateusz Siuda <131684931+Mateusz627@users.noreply.github.com>
2023-09-01 12:32:51 +0200

    Added SearchInput.tsx file containing the SearchInput component for handling user input and redirection to the search results page based on the entered query.

--- app/components/SearchInput.tsx
+++ app/components/SearchInput.tsx
@@ -1,10 +1,35 @@
 "use client";
 
+import {useRouter} from "next/navigation";
+import {useEffect, useState} from "react";
+import useDebounce from "@/hooks/useDebounce";
+import qs from "query-string";
+import Input from "@/app/components/Input";
+
 const SearchInput = () => {
+    const router = useRouter();
+    const [value, setValue] = useState<string>("");
+    const debouncedValue = useDebounce<string>(value, 500);
+
+    useEffect(() => {
+        const query = {
+            title: debouncedValue,
+        };
+
+        const url = qs.stringifyUrl({
+            url: '/search',
+            query: query
+        });
+
+        router.push(url);
+    }, [debouncedValue, router]);
+
   return(
-    <div>
-        asd
-    </div>
+    <Input
+        placeholder="What do you want to listen to ?"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+    />
   );
 }
 
