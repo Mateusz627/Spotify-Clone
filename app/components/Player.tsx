f9e0f0d9ed881f94353c735a9b642f41de19fdae
Mateusz Siuda <131684931+Mateusz627@users.noreply.github.com>
2023-09-05 11:58:36 +0200

    Added useGetSongById.ts file containing the custom hook useGetSongById for fetching song information based on its ID.

---
+++ app/components/Player.tsx
@@ -0,0 +1,14 @@
+"use client"
+import usePlayer from "@/hooks/usePlayer";
+
+const Player = () => {
+    const player = usePlayer();
+
+  return(
+      <div>
+        Player!
+      </div>
+  )
+}
+
+export default Player;
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import UserProvider from "@/providers/UserProvider";
 import ModalProvider from "@/providers/ModalProvider";
 import ToasterProvider from "@/providers/ToasterProvider";
 import getSongsByUserId from "@/actions/getSongsByUserId";
+import Player from "@/app/components/Player";
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -32,6 +33,7 @@ export default async function RootLayout({
                   <Sidebar songs={userSongs}>
                       {children}
                   </Sidebar>
+                  <Player />
               </UserProvider>
           </SupabaseProvider>
       </body>
