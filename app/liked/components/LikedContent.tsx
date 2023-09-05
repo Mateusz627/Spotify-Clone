bd35153b8252a73b15f2a6d121645e063264a632
Mateusz Siuda <131684931+Mateusz627@users.noreply.github.com>
2023-09-05 10:21:41 +0200

    Added LikedContent.tsx file containing the LikedContent component for displaying a list of liked music tracks on the "Liked" page.

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -1,7 +1,59 @@
-const LikedContent = () => {
+"use client";
+
+import {Song} from "@/types";
+import {useRouter} from "next/navigation";
+import {useUser} from "@/hooks/useUser";
+import {useEffect} from "react";
+import MediaItem from "@/app/components/MediaItem";
+import LikeButton from "@/app/components/LikeButton";
+
+interface LikedContentProps {
+    songs: Song[];
+}
+
+const LikedContent: React.FC<LikedContentProps> = ({
+    songs
+}) => {
+    const router = useRouter();
+    const { isLoading, user } = useUser();
+
+    useEffect(() => {
+        if (!isLoading && !user){
+            router.replace('/')
+        }
+    }, [isLoading, user, router]);
+
+    if (songs.length === 0) {
+        return (
+            <div className="
+                flex
+                flex-col
+                gap-y-2
+                w-full
+                px-6
+                text-neutral-400
+            ">
+                No liked songs.
+            </div>
+        )
+    }
+
     return(
-        <div>
-            LikedContent
+        <div className="flex flex-col gap-y-2 w-full p-6">
+            {songs.map((song) => (
+                <div
+                    key={song.id}
+                    className="flex items-center gap-x-4 w-full"
+                >
+                    <div className="flex-1">
+                        <MediaItem
+                            onClick={() => {}}
+                            data={song}
+                        />
+                    </div>
+                    <LikeButton songId={song.id} />
+                </div>
+            ))}
         </div>
     )
 }
